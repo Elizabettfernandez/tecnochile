b34a34f953663eb7a9573e70893f2a84e278c198
Elizabett <e.fernandezallende@gmail.com>
2022-03-30 11:21:04 -0300

    Merge branch 'main' of https://github.com/Elizabettfernandez/tecnochile into elizabett

---
+++ actualizacion 2/tecnochile-main/javascript.js
@@ -0,0 +1,32 @@
+function myFunction() {
+
+    let nombre = prompt('ingresa tu nombre');
+    let apellido = prompt('ingresa tu apellido');
+    
+    console.log(nombre+" "+apellido);
+    
+    let datos = document.getElementById('clientes');
+    
+    datos.innerHTML= `${nombre} ${apellido}`; 
+    }
+
+    //contador
+    
+    let numero = 0;
+    function sumar(){
+        ++numero;
+        console.log(numero);
+        let contador = document.getElementById('mensaje');
+        contador.innerHTML = numero;
+       if(numero >= 20){document.getElementById("mensaje").style.color="green";}
+       else{document.getElementById("mensaje").style.color="black";}
+}
+
+    function restar() {
+        --numero;
+        console.log(numero);
+        let contador = document.getElementById('mensaje');
+        contador.innerHTML = numero;
+        if(numero < 0){document.getElementById("mensaje").style.color="red";}
+        else{document.getElementById("mensaje").style.color="black";}
+    }
